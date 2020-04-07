56b8d7bcb6de0b09c8f4470a6a8546ecdef1421b
amal jayaraj <ajayaraj@architrave.de>
2020-04-07 19:38:16 +0200

    add 1st test

--- test.ts
+++ test.ts
@@ -2,6 +2,24 @@ import { Selector } from "testcafe";
 
 fixture("currency convertor").page("https://cash-conversion.dev-tester.com/");
 
+const fromCurrency = Selector("#from_currency");
+const fromCurrencyOptions = fromCurrency.find("option");
+const toCurrency = Selector("#to_currency");
+const toCurrencyOptions = toCurrency.find("option");
+const conversionMsg = Selector(".conversion-response");
+
 test("Should be able to convert currency", async (t) => {
-    await t.maximizeWindow;
+  await t.maximizeWindow;
+
+  await t
+    .typeText("#base_amount", "100")
+    .click(fromCurrency)
+    .click(fromCurrencyOptions.withText("Silver Ounce"))
+    .click(toCurrency)
+    .click(toCurrencyOptions.withText("CFP Franc"))
+    .click("#convert_btn");
+
+  await t
+    .expect(conversionMsg.innerText)
+    .eql("100 Silver Ounce is about 204021.38 CFP Franc");
 });
